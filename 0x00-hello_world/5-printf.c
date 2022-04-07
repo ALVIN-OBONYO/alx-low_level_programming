1c444ba751eb56bc264cd0f0c32a6b17d600248f
Alvin Obonyo <alvinnmichael@gmail.com>
2022-04-07 16:34:37 -0700

    rain

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main -main block
- * Return 0
+ * Return: 0
  **/
+int main (void)
+{
 printf("with proper grammer, both the outcome is a piece of art, \n")
 return (0);
 }
