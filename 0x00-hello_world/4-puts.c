b9b2ba509647f4dd83f7abce9aa37be71ec1f512
Alvin Obonyo <alvinnmichael@gmail.com>
2022-04-07 16:05:38 -0700

    fri

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry point
  * 
  */
-int main (void)
+int main(void)
 {
-	puts ("\" Programming is like bulding a multilingual puzzle");
-	return (0);
+puts ("\" Programming is like bulding a multilingual puzzle");
+return (0);
 }
