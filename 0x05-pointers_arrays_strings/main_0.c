c2ca2b93fd9be6f69e07d3567ed225d3af89c887
ALVIN-OBONYO'git config --global user.name ALVIN-OBONYO <alvinnmichael@gmail.com>
2022-04-21 02:56:13 +0300

    tik

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int _atoi(char *)
+
+/**
+ * main - check my code
+ *
+ * Return: Is 0.
+ */
+int main(void)
+{
+	int n;
+
+	n= _atoi("4");
+	printf("%d\n", n);
+	return (0);
+}
+
