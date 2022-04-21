e1380ec7f7cfe699d5971f746994079fd93a9a8c
ALVIN-OBONYO'git config --global user.name ALVIN-OBONYO <alvinnmichael@gmail.com>
2022-04-21 03:19:56 +0300

    griffin

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,20 +3,21 @@
 #include <string.h>
 
 /**
- * puts2 - prints a string, followed by a new line,
- * @str: pointer to the new string to print
- * Return: Always 0.
- */
+ *  * puts2 - prints every other character of a string
+ *   * followed by a new line
+ *    *@str:input
+ *     * Return: Always 0.
+*/
 void puts2(char *str)
 {
-	int i = 0;
-	while (str[i] != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 		}
-		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
