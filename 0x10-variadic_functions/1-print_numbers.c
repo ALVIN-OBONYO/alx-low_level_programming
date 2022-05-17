452ebf323ca2f207597bf77fe5c41477af88a3b4
ALVIN-OBONYO'git config --global user.name ALVIN-OBONYO <alvinnmichael@gmail.com>
2022-05-18 01:53:34 +0300

    correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
- * print_numbers - prints numbers, followed by a new line
- * @separator: str to be printed btwn nums
- * @n: num of ints passed as args
+ *print_numbers-Prints numbers followed by a new line
+ *@separator: parameter 1
+ *@n: Parameter 2
+ *return: Nothing
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list list;
+	va_list args;
 
-	va_start(list, n);				/* start iterating args */
-
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(list, int));	/* va_arg -> additional arg */
-		if (i != (n - 1) && separator != NULL)
+		printf("%d", va_arg(args, int));
+		if (i < (n - 1) && separator != NULL)
+		{
 			printf("%s", separator);
+		}
 	}
-
-	printf("\n");
-
-	va_end(list);
+	va_end(args);
+			printf("\n");
 }
