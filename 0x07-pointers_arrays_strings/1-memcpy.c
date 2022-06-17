6262966432e41a41e7c15ec2bea76b9a2b7abc12
ALVIN-OBONYO'git config --global user.name ALVIN-OBONYO <alvinnmichael@gmail.com>
2022-06-18 01:23:24 +0300

    ok

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,23 +1,20 @@
 #include "main.h"
-/**
- * _memcpy - a function that copies memory area
- * @dest: memory where is stored
- * @src: memory where is copied
- * @n: number of bytes
- *
- * Return: copied memory with n byted changed
- */
-char *_memcpy(char *dest, char *src, unsiged int n)
-{
-	int i = 0;
-	int j = 0;
 
-	while (n > 0)
-	{ 
-		dest[i] = src[j];
-		i++;
-		j++;
-		n++;
+/**
+ * _memcpy - irontnoux2
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
 	}
 	return (dest);
 }
