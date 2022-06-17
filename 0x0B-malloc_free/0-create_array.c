a4c8eda3aff6512e2b1e7ec1b163a2c059766a8e
ALVIN-OBONYO'git config --global user.name ALVIN-OBONYO <alvinnmichael@gmail.com>
2022-06-18 00:08:18 +0300

    o

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,38 +1,69 @@
 #include "main.h"
-/**
- * create_array - array for prints a string
- * @size: number elements array
- * @c: char
- * Return: pointer
- */
+#include <stdlib.h>
 
+/**
+ * create_array - allocate space for an array
+ * @size:unssigned int
+ * @c:char
+ * Return:char
+ */
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
 
-	if (size == 0)
-	{
+unsigned int i;
+char *x;
+
+if (size == 0)
+return (NULL);
+x = malloc(size * sizeof(char));
+if (x == NULL)
+{
+return (NULL);
+}
+trtow - splits sentence into words
+* @str: the sentence string
+* *
+* * Return: pointer to string array
+* */
+char **strtow(char *str)
+{
+	int words = 0, in_word = 0;
+	char **ret, *word_start;
+	
+	if (!str || !*str || !wordcount(str))
 		return (NULL);
-	}
-
-	/*Define values with malloc*/
-	buffer = (char *) malloc(size * sizeof(c));
-
-	if (buffer == 0)
+	ret = malloc(sizeof(char *) * (wordcount(str) + 1));
+	while (1)
 	{
-		return (NULL);
-	}
-
-	else
-	{
-		position = 0;
-		while (position < size) /*While for array*/
+		if (*str == ' ' || !*str)
 		{
-			*(buffer + position) = c;
-			position++;
+			if (in_word)
+			{
+				ret[words] = malloc(sizeof(char) * (in_word + 1));
+				if (!ret[words])
+				{
+					return (NULL);
+				}
+				copychars(ret[words], word_start, str - 1);
+				words++;
+				in_word = 0;
+			}
+			if (!*str)
+				break;
 		}
-
-		return (buffer);
+		else
+		{
+			if (!in_word++)
+				word_start = str;
+		}
+		str++;
 	}
+	ret[words] = 0;
+	return (ret);
+}
+or (i = 0; i < size; i++)
+{
+x[i] = c;
+}
+return (x);
 }
